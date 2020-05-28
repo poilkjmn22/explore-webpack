5f28fec41999bd0a5bd7fa37b9e2d40d71e34a1d
qi.fang <qi.fang@wenba100.com>
2020-05-28 18:01:01 +0800

    update: webpack.analyze.js

--- build/webpack.analyze.js
+++ build/webpack.analyze.js
@@ -4,7 +4,14 @@ const base = require('./webpack.base.js');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(base, {
+    module: {
+        rules: [{
+            test: /\.(sa|sc|c)ss$/i,
+            exclude: /(node_modules|bower_components)/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+        }]
+    },
     plugins: [new BundleAnalyzerPlugin({
-      analyzerMode: 'server'
+        analyzerMode: 'server'
     })]
 })
